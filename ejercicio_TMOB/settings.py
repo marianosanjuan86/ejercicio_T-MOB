7d78073eb4d672bc7a959f174b1b18621fb77a93
Mariano San Juan <s.jmariano@gmail.com>
2022-06-14 16:10:50 -0300

    configura base de datos MySQL

--- ejercicio_TMOB/settings.py
+++ ejercicio_TMOB/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ejercicio_TMOB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("NAME_DB"),
+        'HOST': os.environ.get("HOST_DB"),
+        'USER': os.environ.get("USER_DB"),
+        'PASSWORD': os.environ.get("PASSWORD_DB"),
+        'PORT': '',
     }
 }
 
