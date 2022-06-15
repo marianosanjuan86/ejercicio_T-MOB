d0378f22fe631bd0e9129fafab75a2ec0ebca058
Mariano San Juan <s.jmariano@gmail.com>
2022-06-14 23:53:44 -0300

    agrega signal

--- app_ejercicio/models.py
+++ app_ejercicio/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.cache import cache
+import json
 
 class Redirect (models.Model):
     key = models.CharField(max_length=25)
@@ -6,3 +10,15 @@ class Redirect (models.Model):
     active = models.BooleanField(default=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+@receiver(post_save, sender=Redirect, )
+def guardar_en_cache (sender, instance, **kwargs):
+    if instance.active:
+        datos = {
+            "key" : instance.key,
+            "url" : instance.url,
+        }
+        cache.set(instance.key, json.dumps(datos) , 60*60*60)
+        return     
+
+
